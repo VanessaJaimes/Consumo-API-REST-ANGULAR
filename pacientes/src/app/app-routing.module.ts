353471b845849ecdf08901d32b498d429c3f4f96
Vanessa <ladyvane1412@gmail.com>
2023-10-11 20:15:16 -0500

    Rutas configuradas

---
+++ pacientes/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'nuevo', component: NuevoComponent },
+  { path: 'editar', component: EditarComponent }
+];
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, DashboardComponent, NuevoComponent, EditarComponent]
