fd9f0e27cba82efa12bffad54dee418143eef6f3
Vanessa <ladyvane1412@gmail.com>
2023-10-12 08:37:27 -0500

    Finalizacion FormGroup y FormControl

--- pacientes/src/app/app.module.ts
+++ pacientes/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    routingComponents
+    routingComponents,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pacientes/src/app/vistas/login/login.component.ts
+++ pacientes/src/app/vistas/login/login.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginForm = new FormGroup({
+    usuario : new FormControl('', Validators.required),
+    password : new FormControl('', Validators.required)
+  })
+  constructor(){ }
+
+  ngOnInit(): void{
+
+  }
+  onLogin(form : any){
+    console.log(form);
+   }
 }
